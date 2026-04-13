fe62e27e4a335ad7705435e8130773a726858c2f
Kennett <kaguilarzaldana@student.cscc.edu>
2026-04-12 21:51:41 -0400

    Replaced Starbasenow, ship2(no bg), and laserBlast with pizza bg, freddy, and bullet

--- settings.py
+++ settings.py
@@ -19,14 +19,14 @@ class Settings:
         self.screen_w = 1200
         self.screen_h = 800
         self.FPS = 60
-        self.bg_file = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
+        self.bg_file = Path.cwd() / 'Assets' / 'images' / 'pizza_bg.png'
 
-        self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ship2(no bg).png'
+        self.ship_file = Path.cwd() / 'Assets' / 'images' / 'pizza_freddy.png'
         self.ship_w = 40
         self.ship_h = 60
         self.ship_speed = 5
 
-        self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'laserBlast.png'
+        self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'pizza_bullet.png'
         self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser.mp3'
         self.bullet_speed = 7
         self.bullet_w = 25
