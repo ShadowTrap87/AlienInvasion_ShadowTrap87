4082ad6429169663f677b504104c6bb0d54d2d5f
Kennett <kaguilarzaldana@student.cscc.edu>
2026-04-12 21:53:43 -0400

    Updated scale for pizza_freddy and pizza_bullet

--- settings.py
+++ settings.py
@@ -22,14 +22,14 @@ class Settings:
         self.bg_file = Path.cwd() / 'Assets' / 'images' / 'pizza_bg.png'
 
         self.ship_file = Path.cwd() / 'Assets' / 'images' / 'pizza_freddy.png'
-        self.ship_w = 40
-        self.ship_h = 60
+        self.ship_w = 80
+        self.ship_h = 80
         self.ship_speed = 5
 
         self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'pizza_bullet.png'
         self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser.mp3'
         self.bullet_speed = 7
-        self.bullet_w = 25
+        self.bullet_w = 80
         self.bullet_h = 80
         self.bullet_amount = 5
 
